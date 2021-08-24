361fdcaf460f088d3730e1923a15fc4c4866f74a
John Hadden <59323260+jhadden04@users.noreply.github.com>
2021-08-24 19:33:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import praw
+import time
+reddit = praw.Reddit(client_id='ALeJtHe6ez376kZukbshMA',
+                      client_secret='Zc0Rv4fKX55X6j6_HMtiYae2LrxL0A',
+                     user_agent='a reddit instance',
+                    username='Apprehensive_Roof1',
+                     password='password')
 
-reddit = praw.Reddit(
-    client_id="aCsxA497160GjRop_8DDXA",
-    client_secret="rAuMc2mxJPwLMsX4xYqNrpg5ifa7ig",
-    user_agent="reddit bot for udemy course",
-    username="DistinctCap2574",
-    password="this is the password",
-    check_for_async=False
-)
 
 import random
 import time
 def karma():
   try:
     messages = ["Upvoted, upvote in return?", "Great post, care to share the upvotes!"]
+    """
+    for i in range(5):
+        reddit.subreddit("test").submit(random.randint(1,100), random.randint(1,100000000))
+        time.sleep(3)"""
     for submission in reddit.subreddit("FreeKarma4U+FreeKarma4You").stream.submissions():
       submission.upvote()
       rand = random.randint(0, (len(messages)-1))
